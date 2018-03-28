af8ce968e17197a0f0a4dd80f9cdfebd4fa6ebe3
mjanuario <marcelajanuario92@hotmail.com>
2018-03-28 14:39:41 -0300

    Commit teste Marcela

--- src/main/java/com/twu/exemplo/exemplo/ExemploApplication.java
+++ src/main/java/com/twu/exemplo/exemplo/ExemploApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExemploApplication {
 
+	private int num;
+	private int idade;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ExemploApplication.class, args);
 	}
